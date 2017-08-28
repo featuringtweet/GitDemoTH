a6364c2c35eac4a9da7bd2dd17c21fc85ef41b2c
Joshua Lee <JoshuaLee@macbooks-macbook-pro.local>
2017-08-28 17:43:13 -0400

    Initial Commit

---
+++ GitDemoTH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemoTH
+//
+//  Created by Joshua Lee on 8/28/17.
+//  Copyright © 2017 Joshua Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
