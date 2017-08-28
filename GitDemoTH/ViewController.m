8848a88bad2bca2424224f4bcfbdcf4f0285bb47
Joshua Lee <HuaInc@yahoo.com>
2017-08-28 18:17:09 -0400

    test label text change

--- GitDemoTH/ViewController.m
+++ GitDemoTH/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.testLabel.text = @"Hello, World!";
+    self.testLabel.text = @"Hua";
 }
 
 
